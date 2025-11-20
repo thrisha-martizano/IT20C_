8f02dfb38895b9532f9468c15245bfb806c8be0e
Thrisha-MArtizano <martizanothrisha@gmail.com>
2025-11-20 08:31:06 +0800

    Added while-loop.js

---
+++ while-loop.js
@@ -0,0 +1,34 @@
+/*console.log("Descending");
+i = 5;
+
+while(i >= 1){
+    console.log(i);
+    i--;
+}
+
+console.log("Ascending");
+x = 1;
+
+while(x <= 5){
+    console.log(x);
+    x++;
+}
+
+
+output = " ";
+x = 1;
+
+while(x <= 5){
+    output += x + " ";
+    x++;
+} console.log(output);
+*/
+
+arr =[1,2,3];
+output = " ";
+index = 1;
+
+while(index <= arr.length){
+    output += index + " ";
+    index++;
+}console.log(output);
