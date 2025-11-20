adbb2df25dbfd67dfc2dc840e355bcc9be6cb199
Thrisha-MArtizano <martizanothrisha@gmail.com>
2025-11-20 08:16:09 +0800

    Added for-loop

---
+++ for-loop.js
@@ -0,0 +1,30 @@
+//For Loop
+
+// output = " ";
+// for(i = 1; i<=5; i++){
+//     console.log(i);
+//     output += i + " ";
+    
+// }console.log(output);
+
+//Arrays
+/*output = " ";
+arr = [1,2,3]
+
+for(index=0; index<arr.length; index++){
+    //console.log(arr[index]);
+    output += arr[index] + " ";
+} 
+
+console.log(output); */
+
+output = " ";
+arr = [1,2,3]
+console.log("Array Length: "+ arr.length)
+for(index = arr.length -1; index>=0; index--){
+    // console.log(arr[index]);
+    output += arr[index] + " ";
+} 
+
+console.log(output);
+
