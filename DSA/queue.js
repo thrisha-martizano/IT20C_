0cbc39749aa716f437fa2f92c47912c9400beac5
thrisha martizano <martizanothrisha@gmail.com>
2025-12-01 07:51:38 +0800

    Added initial methods for DSA/queue

---
+++ DSA/queue.js
@@ -0,0 +1,27 @@
+class Queue{
+    constructor(){
+        this.data ={};
+        this.frontIndex = 0;
+        this.rearIndex = -1;
+    }
+
+    enqueue(element){
+        this.rearIndex++;
+        this.data[this.rearIndex] = element;
+    }
+
+    traverse(){
+        let output = "";
+        for(let i = this.frontIndex; i <= this.rearIndex; i++){
+            output += this.data[i] + " ";
+        }
+
+        console.log(output);
+    }
+}
+
+let queue1 = new Queue();
+queue1.enqueue(4);
+queue1.enqueue(2);
+queue1.enqueue(3);
+queue1.traverse();
