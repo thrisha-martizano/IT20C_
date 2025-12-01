537e626ca242e2652371bf7bfa632ac4cd54b5fe
thrisha martizano <martizanothrisha@gmail.com>
2025-12-01 08:01:33 +0800

    Added utility methods for DSA/queue

--- DSA/queue.js
+++ DSA/queue.js
@@ -18,10 +18,28 @@ class Queue{
 
         console.log(output);
     }
+
+    is_empty(){
+        return this.frontIndex > this.rearIndex;
+    }
+
+    front(){
+        //Correction Statement
+        if(this.is_empty()) return null;
+        return this.data[this.frontIndex];
+    }
+
+    size(){
+        return this.rearIndex - this.frontIndex +1;
+    }
 }
 
+//
 let queue1 = new Queue();
 queue1.enqueue(4);
 queue1.enqueue(2);
 queue1.enqueue(3);
-queue1.traverse();
+// queue1.traverse();
+console.log(queue1.is_empty());
+console.log(queue1.front());
+console.log(queue1.size());
