e9f38851ee1890291efe709c4108cab176d579ee
thrisha martizano <martizanothrisha@gmail.com>
2025-12-01 08:41:07 +0800

    Added insert after methods for DSA/linkedlist.js

--- DSA/linkedlist.js
+++ DSA/linkedlist.js
@@ -42,6 +42,21 @@ class LinkedList{
 
         current.next = node;
     }
+
+    insert_after(target,element){
+        let current = this.head;
+        while(current){
+            if(current.value === target){
+                const node = new Node(element);
+                node.next = current.next;
+                current.next = node;
+                return;
+            }
+            current = current.next;
+        }
+
+        console.log("Target not found");
+    }
 }
 
 //
@@ -49,8 +64,14 @@ let list1 = new LinkedList();
 // list1.insert_at_beginning(4);
 // list1.insert_at_beginning(2);
 // list1.insert_at_beginning(3);
+// console.log(list1.traverse());
+
+// list1.insert_at_end(4);
+// list1.insert_at_end(2);
+// list1.insert_at_end(3);
+// console.log(list1.traverse());
 
 list1.insert_at_end(4);
-list1.insert_at_end(2);
+list1.insert_after(4,2);
 list1.insert_at_end(3);
 console.log(list1.traverse());
