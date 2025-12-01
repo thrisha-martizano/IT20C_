3925171a7129a10db97624834ba8d4f761e64a7a
thrisha martizano <martizanothrisha@gmail.com>
2025-12-01 08:31:41 +0800

    Added insert at end methods for DSA/linkedlist.js

--- DSA/linkedlist.js
+++ DSA/linkedlist.js
@@ -26,11 +26,31 @@ class LinkedList{
         }
         return output;
     }
+
+    insert_at_end(element){
+        const node = new Node(element);
+
+        if(!this.head){
+            this.head = node;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next){
+            current = current.next;
+        }
+
+        current.next = node;
+    }
 }
 
 //
 let list1 = new LinkedList();
-list1.insert_at_beginning(4);
-list1.insert_at_beginning(2);
-list1.insert_at_beginning(3);
+// list1.insert_at_beginning(4);
+// list1.insert_at_beginning(2);
+// list1.insert_at_beginning(3);
+
+list1.insert_at_end(4);
+list1.insert_at_end(2);
+list1.insert_at_end(3);
 console.log(list1.traverse());
